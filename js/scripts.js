786238d5520c17d4b322be64a0f893e98404baa1
Nicholas Muturi <nicholasmuturi1@gmail.com>
2018-07-20 17:30:39 +0300

    inserted if statement to check whether a player is active or not (this determines alot of things)

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,29 @@
 $(document).ready(function(){
   $("form#usernameForm").submit(function(event){
     event.preventDefault();
-
     var playerOneInput = $("input#playerOneInput").val();
     var playerTwoInput = $("input#playerTwoInput").val();
 
     var player1 = new Player(playerOneInput);
     var player2 = new Player(playerTwoInput);
 
-    $("h2#player1Name").text(player1.name);
+    var allPlayers = [player1, player2];
+
+    $("h2#player1Name").text(player1.name).addClass("activePlayer");;
     $("h2#player2Name").text(player2.name);
     $("h2#player1OverallScore").text("0").addClass("animated rollIn");
     $("h2#player2OverallScore").text("0").addClass("animated rollIn");
     clearForm();
 
-  });
+    $("#rollDiceButton").click(function(){
+      if($("h2#player2Name").attr("class")=="activePlayer"){
+        console.log("player is active");
+      }else {
+        console.log("player is not active")
+      }
+    });
 
+  });
   function clearForm(){
     $("input#playerOneInput").val("");
     $("input#playerTwoInput").val("");
