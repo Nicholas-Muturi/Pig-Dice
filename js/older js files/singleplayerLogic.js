04cca505c829897f081d5bedc5bb1be1704fbe3f
Nicholas Muturi <nicholasmuturi1@gmail.com>
2018-07-22 00:33:17 +0300

    Combined both single player and multiplayer logics

--- js/older js files/singleplayerLogic.js
+++ js/older js files/singleplayerLogic.js
@@ -1,19 +1,29 @@
+/*..............FRONT END..............*/
 $(document).ready(function() {
   $("form#usernameForm").submit(function(event) {
     event.preventDefault();
 
     var gameType = "";
     if($('#multiplayerDiv').is(':hidden')){
-      /*.....................SINGLE PLAYER MODE...................*/
+      /*...........SINGLE PLAYER MODE............*/
       gameType = "singlePlayer";
-      startGame();
+
       var playerOneInput = $("input#playerOneInput").val();
       var playerOneStatus = $("#player1Name").attr("class");
       var playerOne = new Player(playerOneInput, playerOneStatus, 0, 0);
       var playerTwo = new Player("Bot ^_^", "", 0, 0);
+
+      $("#player1Name").text(playerOne.name);
+      $("#player2Name").text(playerTwo.name);
+      $("#player1OverallScore").text(playerOne.overallScore).addClass("animated rollIn");
+      $("#player2OverallScore").text(playerTwo.overallScore).addClass("animated rollIn");
+      $("#playerOneTurnScore").text(playerOne.turnScore);
+      $("#playerTwoTurnScore").text(playerOne.turnScore);
+      clearForm();
+
     }
     else {
-      /*....................MULTIPLAYER MODE....................*/
+      /*...........MULTIPLAYER MODE............*/
       gameType = "multiPlayer";
       startGame();
       var playerOneInput = $("input#playerOneInput").val();
@@ -23,7 +33,23 @@ $(document).ready(function() {
 
       var playerOne = new Player(playerOneInput, playerOneStatus, 0, 0);
       var playerTwo = new Player(playerTwoInput, playerTwoStatus, 0, 0);
+
+      $("#player1Name").text(playerOne.name);
+      $("#player2Name").text(playerTwo.name);
+      $("#player1OverallScore").text(playerOne.overallScore).addClass("animated rollIn");
+      $("#player2OverallScore").text(playerTwo.overallScore).addClass("animated rollIn");
+      $("#playerOneTurnScore").text(playerOne.turnScore);
+      $("#playerTwoTurnScore").text(playerOne.turnScore);
+      clearForm();
+
+
     }
+
+    startGame();
+    var playerOneInput = $("input#playerOneInput").val();
+    var playerOneStatus = $("#player1Name").attr("class");
+    var playerOne = new Player(playerOneInput, playerOneStatus, 0, 0);
+    var playerTwo = new Player("Bot ^_^", "", 0, 0);
 
     $("#player1Name").text(playerOne.name);
     $("#player2Name").text(playerTwo.name);
@@ -37,82 +63,42 @@ $(document).ready(function() {
     /*...............ROLL DICE EVENT LISTENER.................*/
     $("#rollDiceButton").click(function() {
       playerOne.status = $("#player1Name").attr("class");
-      playerTwo.status = $("#player2Name").attr("class");
-      var diceRoll;
-
-      /*............UPDATES TURN SCORES FOR EACH PLAYER
-      ..................BASED ON WHO'S TURN IT IS.................*/
       if (playerOne.status == "activeTurn") {
-        diceRoll = playerOne.rollDice();
+        var diceRoll = playerOne.rollDice();
         if (diceRoll != 1) {
           $("#dice").text(diceRoll);
           playerOne.turnScore += diceRoll;
           $("#playerOneTurnScore").text(playerOne.turnScore);
         } else {
           $("#dice").text(diceRoll+"...whoops!");
+          switchPlayerTurn();
           playerOne.turnScore = 0;
           $("#playerOneTurnScore").text("0");
-          switchPlayerTurn();
-
-          if(gameType == "singlePlayer"){
-            computersTurn();
-          }
-        }
-      } else {
-        diceRoll = playerTwo.rollDice();
-        if (diceRoll != 1) {
-          $("#dice").text(diceRoll);
-          playerTwo.turnScore += diceRoll;
-          $("#playerTwoTurnScore").text(playerTwo.turnScore);
-        } else {
-          $("#dice").text(diceRoll+"...whoops!");
-          playerTwo.turnScore = 0;
-          $("#playerTwoTurnScore").text("0");
-          switchPlayerTurn();
+          computersTurn();
         }
       }
     });
 
-    /*...............HOLD BUTTON EVENT LISTENER.................*/
+
     $("#holdButton").click(function() {
-      playerOne.status = $("#player1Name").attr("class");
-      playerTwo.status = $("#player2Name").attr("class");
+        playerOne.status = $("#player1Name").attr("class");
 
-      /*...............UPDATES OVERALL SCORES FOR EACH PLAYER
-      ..................BASED ON WHO'S TURN IT IS.................*/
-      if (playerOne.status == "activeTurn") {
-        playerOne.hold();
-        $("#player1OverallScore").text(playerOne.overallScore);
-        $("#playerOneTurnScore").text(playerOne.turnScore);
-        switchPlayerTurn();
-
-        if(gameType == "singlePlayer"){
+        if (playerOne.status == "activeTurn"){
+          playerOne.hold();
+          $("#player1OverallScore").text(playerOne.overallScore);
+          $("#playerOneTurnScore").text(playerOne.turnScore);
+          switchPlayerTurn();
           computersTurn();
+        } else {
+          switchPlayerTurn();
         }
 
-      } else {
-        playerTwo.hold();
-        $("#player2OverallScore").text(playerTwo.overallScore);
-        $("#playerTwoTurnScore").text(playerTwo.turnScore);
-        switchPlayerTurn();
-      }
-
-      /*...............Checks For Winner.................*/
       if (playerOne.overallScore >= 100) {
         alert(playerOne.name + " is a Winner Winner Chicken Dinner!");
         location.reload();
-
-        if(gameType == "singlePlayer"){
-          location.reload();
-          playerOne.resetData();
-        }
-
-      } else if (playerTwo.overallScore >= 100) {
-        alert(playerTwo.name + " is a Winner Winner Chicken Dinner!");
-        location.reload();
+        playerOne.resetData();
       }
     });
-
 
     function computersTurn(){
       playerTwo.status = $("#player2Name").attr("class");
@@ -142,8 +128,8 @@ $(document).ready(function() {
         }
       }
     }
-
   }); //End of Form Submit
+
   function startGame() {
     $(".landing-menu").hide();
     $(".game-section").show();
@@ -167,6 +153,8 @@ $(document).ready(function() {
     $(".landing-menu").show();
   }
 }); //End of JQuery
+
+
 
 
 /*..............BACK END..............*/
