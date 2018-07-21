ad2a819d07f03e559bd0debe62333c55fb0de196
Nicholas Muturi <nicholasmuturi1@gmail.com>
2018-07-21 13:12:33 +0300

    landing menu design complete and added menu.js file for menu logic

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+  $("#singlePlayer").click(function(){
+    //console.log("here i am");
+
+
+    
+  });
+
+  $("#multiPlayer").click();
+  $("#aboutGame").click();
+
+
+
+
+});
