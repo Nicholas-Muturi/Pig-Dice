cd445a12fd29a3b7d6809adcb27d051d79266eee
Nicholas Muturi <nicholasmuturi1@gmail.com>
2018-07-21 13:58:42 +0300

    added functionality for single player and multiplayer buttons: shows and reveals form inputs based on selection

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,24 @@
 $(document).ready(function(){
-
+  $("#usernameForm").hide();
   $("#singlePlayer").click(function(){
     //console.log("here i am");
-
-
-    
+    $("#usernameForm").show().slideDown("slow");
+    $("#singleplayerDiv").removeClass("col-md-6");
+    $("#singleplayerDiv").addClass("col-md-12");
+    $('#multiplayerDiv').css('display','none');
   });
 
-  $("#multiPlayer").click();
-  $("#aboutGame").click();
+  $("#multiPlayer").click(function(){
+    $("#usernameForm").show().slideDown("slow");
+    $('#multiplayerDiv').css('display','block');
+    $("#singleplayerDiv").removeClass("col-md-12");
+    $("#singleplayerDiv").addClass("col-md-6");
+  });
+
+  $("#aboutGame").click(function(){
+    $(".landing-menu").slideUp();
+    $("#usernameForm").slideUp();
+  });
 
 
 
