4683c89b5f3735aa585f60dc0677997d7f39865f
Nicholas Muturi <nicholasmuturi1@gmail.com>
2018-07-22 00:03:25 +0300

    added bot to compete with user in single player (this wasn't easy)

--- js/singleplayerLogic.js
+++ js/singleplayerLogic.js
@@ -3,14 +3,25 @@ $(document).ready(function() {
   $("form#usernameForm").submit(function(event) {
     event.preventDefault();
 
+    if(true){
+      /*...........SINGLE PLAYER MODE............*/
+
+    }
+    else {
+      /*...........MULTIPLAYER MODE............*/
+
+
+
+
+    }
+
+
+
     startGame();
     var playerOneInput = $("input#playerOneInput").val();
-    var playerTwoInput = $("input#playerTwoInput").val();
     var playerOneStatus = $("#player1Name").attr("class");
-    var playerTwoStatus = $("#player2Name").attr("class");
-
     var playerOne = new Player(playerOneInput, playerOneStatus, 0, 0);
-    var playerTwo = new Player(playerTwoInput, playerTwoStatus, 0, 0);
+    var playerTwo = new Player("Bot ^_^", "", 0, 0);
 
     $("#player1Name").text(playerOne.name);
     $("#player2Name").text(playerTwo.name);
@@ -21,72 +32,74 @@ $(document).ready(function() {
     clearForm();
 
 
-
     /*...............ROLL DICE EVENT LISTENER.................*/
     $("#rollDiceButton").click(function() {
       playerOne.status = $("#player1Name").attr("class");
-      playerTwo.status = $("#player2Name").attr("class");
-      var diceRoll;
-
-      /*............UPDATES TURN SCORES FOR EACH PLAYER
-      ..................BASED ON WHO'S TURN IT IS.................*/
       if (playerOne.status == "activeTurn") {
-        diceRoll = playerOne.rollDice();
+        var diceRoll = playerOne.rollDice();
         if (diceRoll != 1) {
           $("#dice").text(diceRoll);
           playerOne.turnScore += diceRoll;
           $("#playerOneTurnScore").text(playerOne.turnScore);
         } else {
-          $("#dice").text(diceRoll);
+          $("#dice").text(diceRoll+"...whoops!");
+          switchPlayerTurn();
           playerOne.turnScore = 0;
           $("#playerOneTurnScore").text("0");
+          computersTurn();
+        }
+      }
+    });
+
+
+    $("#holdButton").click(function() {
+        playerOne.status = $("#player1Name").attr("class");
+
+        if (playerOne.status == "activeTurn"){
+          playerOne.hold();
+          $("#player1OverallScore").text(playerOne.overallScore);
+          $("#playerOneTurnScore").text(playerOne.turnScore);
+          switchPlayerTurn();
+          computersTurn();
+        } else {
           switchPlayerTurn();
         }
-      } else {
-        diceRoll = playerTwo.rollDice();
-        if (diceRoll != 1) {
-          $("#dice").text(diceRoll);
-          playerTwo.turnScore += diceRoll;
-          $("#playerTwoTurnScore").text(playerTwo.turnScore);
-        } else {
-          $("#dice").text(diceRoll);
-          playerTwo.turnScore = 0;
+
+      if (playerOne.overallScore >= 100) {
+        alert(playerOne.name + " is a Winner Winner Chicken Dinner!");
+        location.reload();
+        playerOne.resetData();
+      }
+    });
+
+    function computersTurn(){
+      playerTwo.status = $("#player2Name").attr("class");
+      if(playerTwo.status == "activeTurn"){
+        for(var x=1;x<3;x++){
+          var diceRoll = playerTwo.rollDice();
+          if (diceRoll != 1) {
+            $("#dice").text(diceRoll);
+            playerTwo.turnScore += diceRoll;
+            $("#playerTwoTurnScore").text(playerTwo.turnScore);
+          }
+          else {
+            $("#dice").text(diceRoll+"...whoops!");
+            playerTwo.turnScore = 0;
+            $("#playerTwoTurnScore").text("0");
+          }
+        }
+        playerTwo.hold();
+        if (playerTwo.overallScore >= 100) {
+          alert(playerTwo.name + " is a Winner Winner Chicken Dinner!");
+          location.reload();
+          playerTwo.resetData();
+        }else{
+          $("#player2OverallScore").text(playerTwo.overallScore);
           $("#playerTwoTurnScore").text("0");
           switchPlayerTurn();
         }
       }
-    });
-
-    /*...............HOLD BUTTON EVENT LISTENER.................*/
-    $("#holdButton").click(function() {
-      playerOne.status = $("#player1Name").attr("class");
-      playerTwo.status = $("#player2Name").attr("class");
-
-      /*...............UPDATES OVERALL SCORES FOR EACH PLAYER
-      ..................BASED ON WHO'S TURN IT IS.................*/
-      if (playerOne.status == "activeTurn") {
-        playerOne.hold();
-        $("#player1OverallScore").text(playerOne.overallScore);
-        $("#playerOneTurnScore").text(playerOne.turnScore);
-        switchPlayerTurn();
-      } else {
-        playerTwo.hold();
-        $("#player2OverallScore").text(playerTwo.overallScore);
-        $("#playerTwoTurnScore").text(playerTwo.turnScore);
-
-        switchPlayerTurn();
-      }
-
-      /*...............Checks For Winner.................*/
-      if (playerOne.overallScore >= 100) {
-        alert(playerOne.name + " is a Winner Winner Chicken Dinner!");
-        resetGame();
-      } else if (playerTwo.overallScore >= 100) {
-        alert(playerTwo.name + " is a Winner Winner Chicken Dinner!");
-        resetGame();
-      }
-    });
-
+    }
   }); //End of Form Submit
 
   function startGame() {
@@ -133,3 +146,10 @@ Player.prototype.hold = function() {
   this.overallScore += this.turnScore;
   this.turnScore = 0;
 };
+
+Player.prototype.resetData = function(){
+  this.name = "";
+  this.status = "";
+  this.overallScore = "";
+  this.turnScore = ""
+}
